d801bbf49584b7a759842370613c18837086baa9
Michael Chukwu <naemugo@gmail.com>
2021-11-24 12:46:32 +0100

    all

--- routes/web.php
+++ routes/web.php
@@ -179,7 +179,7 @@ use Laravel\Fortify\Http\Controllers\VerifyEmailController;
 Route::get('/', function (HttpRequest $request) {
     $jobs = Job::when($request->term, function ($query, $term){
         $query->where('title', 'LIKE', '%'.$term.'%');
-    })->where('is_live', true)->get();
+    })->where('is_live', true)->paginate(100);
     if($request->wantsJson()){
         return $jobs;
     }
